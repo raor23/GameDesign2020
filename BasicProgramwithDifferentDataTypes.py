263c026c729813d06d7fcd22d6d8ab316cb12a2d
Rohan Rao <rohan@Rohans-MacBook-Pro.local>
2020-09-14 10:52:37 -0500

    9-14-20

--- BasicProgramwithDifferentDataTypes.py
+++ BasicProgramwithDifferentDataTypes.py
@@ -1,10 +1,14 @@
 # Rohan Rao
-x=9
-y=2
-x = y + 7
-print(x)
-
-x=10
-y = 3
-x=30/y
-print (x)
+x=6
+y=5.0
+a="John"
+b=False
+print(type(x))
+print(type(y))
+print(type(a))
+print(type(b))
+print(x-y)
+print(x+y)
+print(x/y)
+print(x*y)
+print
