799176984d0b0343e043028eefacf35660f4080e
Rohan Rao <rohan@Rohans-MacBook-Pro.local>
2020-09-14 11:39:39 -0500

    Class programs 9-14-20

---
+++ RohanRaoBranching1.py
@@ -0,0 +1,20 @@
+# Rohan Rao
+# This program is to learn how to branch
+# 9/14/20
+# DECLARE VARIABLES
+a=7
+b=13
+c=14
+d=13
+if a%2 == 0:
+    print(b+c)
+elif b%2 == 0:
+    print(c-d)
+else:
+    print(d*c/a)
+# else:
+#     print("a is odd")
+# if b%2 == 0:
+#     print(c-d)
+# else:
+#     print(d*c/a)
