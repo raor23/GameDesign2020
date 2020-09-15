14d18ea91b3441c0a656c0040810715af5f458b2
Rohan Rao <rohan@Rohans-MacBook-Pro.local>
2020-09-15 17:47:27 -0500

    9-16-20 HW

---
+++ ForLoopsHw.py
@@ -0,0 +1,33 @@
+print ("1 2 3 4 5 6 7 8 9")
+print("1\n"+"2\n"+"3\n"+"4\n"+"5\n"+"6\n"+"7\n"+"8\n"+"9\n")
+print("1\n")
+print("2 2\n")
+print("3 3 3\n")
+print("4 4 4 4\n")
+print("5 5 5 5 5\n")
+print("6 6 6 6 6 6\n")
+print("7 7 7 7 7 7 7\n")
+print("8 8 8 8 8 8 8 8\n")
+print("9 9 9 9 9 9 9 9 9\n")
+
+
+
+
+
+
+
+n = range("1,10")
+for i in range(1,n+1):
+    for j in range(1, i+1):
+        print i
+
+
+numbers = ["1 2 3 4 5 6 7 8 9"]
+numbers2 = ["1","2","3","4","5","6","7","8","9"]
+
+for x in numbers:
+    print x
+    continue
+for x in numbers2:
+    print x
+    continue
