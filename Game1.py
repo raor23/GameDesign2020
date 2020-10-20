a8818dee2e6e18048c55160077e548736c3f0bfa
Rohan Rao <rohan@Rohans-MacBook-Pro.local>
2020-10-20 11:29:26 -0500

    10-20-20

--- Game1.py
+++ Game1.py
@@ -1,82 +1,63 @@
+#Rohan Rao
+
 import random
 import time
 
-# library that we use in order to choose
-# on random words from a list of words
-
 name = input("What is your name? ")
-# Here the user is asked to enter the name first
 
 print("Good Luck ! ", name)
 
-words = ['python', 'java','PHP','javaScript',
-'computer', 'geeks', 'keyboard','laptop', 'headphone',
-'hardware','software','dashboard','cache', 'data','desktop','firewall',
-'Internet']
+# Word bank
+# Words to guess from
+words = ['banana','strawberry','mango','blueberry',
+'pineapple','apple','raspberry','blackberry','orange','pear',
+'grape','peach','kiwi']
 
-# Function will choose one random
-# word from this list of words
+#Chooses randomn word
 word = random.choice(words)
-
-
 print("Guess the characters")
+hangman = "  0"
+hangman2 = "\n/"
+hangman3 = "|"
+hangman4 = "\ "
 
+hangman5 = "\n /"
+
+hangman6 = "\ "
+
+print(hangman, hangman2, hangman3, hangman4, hangman5, hangman6)
+
+print("The Theme is Fruits")
 guesses = ''
-
-# any number of turns can be used here
-turns = 12
-
-
+turns = 6
 while turns > 0:
-
-    # counts the number of times a user fails
     failed = 0
-
 
     for char in word:
 
         if char in guesses:
-            print(char)
-
+            print(char, end = " ")
         else:
-            print("_")
-
+            print("_", end = " ")
             failed += 1
 
-
-
-
-
     if failed == 0:
-        # user will win the game if failure is 0
-        # and 'You Win' will be given as output
-        print("You Win")
-
-        # this print the correct word
-        print("The word is: ", word)
+        print("You Win!")
+        print("The word is:", word)
         break
 
-    # if user has input the wrong alphabet then
-    # it will ask user to enter another alphabet
     guess = input("guess a character:")
 
-    # every input character will be stored in guesses
     guesses += guess
-
-    # check input with the character in word
+# If the letter guessed is not in the word it removes a turn
     if guess not in word:
 
         turns -= 1
 
-        # if the character doesn’t match the word
-        # then “Wrong” will be given as output
         print("Wrong")
-
-        # this will print the number of
-        # turns left for the user
         print("You have", + turns, 'more guesses')
 
-
-        if turns == 0:
-            print("GAME OVER YOU LOSE! The word was", word)
-            print("Would you like to play again? ")
+# When user runs out of turns
+if turns == 0:
+    print("GAME OVER YOU LOSE! The word was", word)
+    print("Would you like to play again? ")
